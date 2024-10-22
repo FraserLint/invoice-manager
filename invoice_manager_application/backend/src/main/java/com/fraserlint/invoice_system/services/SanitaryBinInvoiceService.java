535b507e0b3b018127496464049612cd38a5b679
Austin Davison <davisonaustin287@gmail.com>
2024-10-22 23:02:58 +0100

    Added sanitary bin invoice service

---
+++ invoice_manager_application/backend/src/main/java/com/fraserlint/invoice_system/services/SanitaryBinInvoiceService.java
@@ -0,0 +1,47 @@
+package com.fraserlint.invoice_system.services;
+
+import com.fraserlint.invoice_system.entities.SanitaryBinInvoice;
+import com.fraserlint.invoice_system.repos.SanitaryBinInvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class SanitaryBinInvoiceService {
+
+    @Autowired
+    private SanitaryBinInvoiceRepository sanitaryBinInvoiceRepository;
+
+    // Add a new invoice
+    public SanitaryBinInvoice addInvoice(SanitaryBinInvoice invoice){
+        return sanitaryBinInvoiceRepository.save(invoice);
+    }
+
+    // Get all invoices for the tax year
+    public List<SanitaryBinInvoice> getInvoicesForTaxYear(int year) {
+        LocalDate startDate = LocalDate.of(year, Month.APRIL, 6);
+        LocalDate endDate = LocalDate.of(year + 1, Month.APRIL, 5);
+        return sanitaryBinInvoiceRepository.findByDateBetween(startDate, endDate);
+    }
+
+    // Get total amount for a specified week
+    public double getTotalForWeek(LocalDate startDate, LocalDate endDate) {
+        return sanitaryBinInvoiceRepository.findByDateBetween(startDate, endDate)
+                .stream()
+                .mapToDouble(SanitaryBinInvoice::getExpense)
+                .sum();
+    }
+
+    // Calculate profit for a tax year
+    public double getTotalProfitForYear(int year) {
+        LocalDate startDate = LocalDate.of(year, Month.APRIL, 6);
+        LocalDate endDate = LocalDate.of(year + 1, Month.APRIL, 5);
+        return sanitaryBinInvoiceRepository.findByDateBetween(startDate, endDate)
+                .stream()
+                .mapToDouble(SanitaryBinInvoice::getProfit)
+                .sum();
+    }
+}
