88b0ebaae329ccd8dc5647719db3c0cb1d46fc5e
Austin Davison <davisonaustin287@gmail.com>
2024-10-23 21:02:27 +0100

    Updated routing module

--- invoice_manager_application/frontend/invoice-manager/src/app/app-routing.module.ts
+++ invoice_manager_application/frontend/invoice-manager/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ScdInvoicesComponent } from './scd-invoices/scd-invoices.component';
+import { SanitaryBinInvoicesComponent } from './sanitary-bin-invoices/sanitary-bin-invoices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/scd-invoices', pathMatch: 'full' },  // default route
+  { path: 'scd-invoices', component: ScdInvoicesComponent },
+  { path: 'sanitary-bin-invoices', component: SanitaryBinInvoicesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
